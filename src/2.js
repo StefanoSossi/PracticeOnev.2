615f961a8113f03a823ab002dd4539cd6215608d
stefano Sossi <tenosossi2012@gmail.com>
2020-04-07 20:33:39 -0400

    Editing file 2.js

--- src/2.js
+++ src/2.js
@@ -1,7 +1,12 @@
 My file Two
 <<<<<<< HEAD
 My file Edition
+<<<<<<< HEAD
 =======
 My new Master file Edition
 >>>>>>> Editing file 2.js
+=======
+>>>>>>> branchOne
+>>>>>>> Editing file 2.js
+My Other Edition
 My Other Edition
